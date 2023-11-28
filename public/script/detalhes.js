3978e12bbf2bc80bd83fe45c44b31b7f71f979c7
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-11-28 10:46:30 -0300

    Atualização da páginas detalhes

--- public/script/detalhes.js
+++ public/script/detalhes.js
@@ -27,6 +27,7 @@ function loadFooter() {
 document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const estabelecimentoId = urlParams.get('id');
+    carregarFotos(estabelecimentoId);
     
     if (estabelecimentoId) {
         fetch(`http://localhost:3000/api/lugares?_id=${estabelecimentoId}`)
@@ -59,10 +60,132 @@ function criarAvaliacao(avaliacao) {
     return `<div class="rating">${estrelasHtml}</div>`;
 }
 
+function bufferToBase64(buf) {
+    let binary = '';
+    const bytes = new Uint8Array(buf);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
+
+function carregarFotos(lugarId) {
+    const carouselContainer = document.getElementById('carouselContainer');
+    carouselContainer.innerHTML = '';
+
+    fetch(`http://localhost:3000/api/fotos-lugares/${lugarId}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                let carouselIndicators = '';
+                let carouselInner = '';
+                let fotoIndex = 0; // Índice para cada foto
+
+                data.forEach(item => {
+                    item.fotos.forEach(foto => {
+                        const base64String = bufferToBase64(foto.data.data);
+                        const isActive = fotoIndex === 0 ? 'active' : ''; // Apenas a primeira foto é 'active'
+                        
+                        carouselIndicators += `<li data-target="#fotosCarousel" data-slide-to="${fotoIndex}" class="${isActive}"></li>`;
+                        carouselInner += `
+                            <div class="carousel-item ${isActive}">
+                                <img class="d-block w-100" src="data:${foto.contentType};base64,${base64String}">
+                            </div>`;
+                        
+                        fotoIndex++;
+                    });
+                });
+
+                carouselContainer.innerHTML = `
+                    <div id="fotosCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            ${carouselIndicators}
+                        </ol>
+                        <div class="carousel-inner">
+                            ${carouselInner}
+                        </div>
+                        <a class="carousel-control-prev" href="#fotosCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#fotosCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>`;
+
+                // Inicializar o carrossel
+                $('#fotosCarousel').carousel();
+            } else {
+                carouselContainer.innerHTML = '<p>Nenhuma foto encontrada para este lugar.</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao carregar fotos:', error);
+            carouselContainer.innerHTML = '<p>Erro ao carregar fotos.</p>';
+        });
+}
+
+function criarCarrosselIndividual(idCarrossel,tamanho , maximo,titulo, itens) {
+    if (!itens || itens.length === 0) {
+        return '<p>Nenhuma informação disponível</p>';
+    }
+
+    let carouselIndicators = '<ol class="carousel-indicators">';
+    let carouselInner = '<div class="carousel-inner">';
+    const maxCardsPorSlide = maximo;
+    
+    for (let i = 0; i < itens.length; i += maxCardsPorSlide) {
+        let slideContent = '';
+        for (let j = i; j < i + maxCardsPorSlide && j < itens.length; j++) {
+            slideContent += `
+                <div class="card" style="flex: 0 0 auto; width: ${tamanho}rem; margin-right: 15px;">
+                    <div class="card-body">
+                        <p class="card-text">${itens[j]}</p>
+                    </div>
+                </div>`;
+        }
+
+        carouselIndicators += `<li data-target="#${idCarrossel}" data-slide-to="${i / maxCardsPorSlide}" class="${i === 0 ? 'active' : ''}"></li>`;
+        carouselInner += `
+            <div class="carousel-item ${i === 0 ? 'active' : ''}">
+                <div class="d-flex">${slideContent}</div>
+            </div>`;
+    }
+
+    carouselIndicators += '</ol>';
+    carouselInner += '</div>';
+
+    return `
+        <div id="${idCarrossel}" class="carousel slide" data-ride="carousel">
+            ${carouselIndicators}
+            ${carouselInner}
+            <a class="carousel-control-prev" href="#${idCarrossel}" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#${idCarrossel}" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>`;
+}
+
 
 
 function criarCardDetalhe(estabelecimento) {
-    console.log(estabelecimento);
+    const carrosselCozinha          = criarCarrosselIndividual('carrosselCozinha',          '12', 4,  'culinária', estabelecimento.cozinha);
+    const carrosselEstiloMusical    = criarCarrosselIndividual('carrosselEstiloMusical',    '12', 4,  'estilo musical', estabelecimento.estilo_musical);
+    const carrosselTipoEvento       = criarCarrosselIndividual('carrosselTipoEvento',       '12', 4,  'tipo de evento', estabelecimento.tipo_evento);
+    const carrosselLocal            = criarCarrosselIndividual('carrosselLocal',            '12', 4,  'locais', estabelecimento.local);
+    const carrosselAmbiente         = criarCarrosselIndividual('carrosselAmbiente',         '12', 4,  'ambientes', estabelecimento.ambiente);
+    const carrosselHobby            = criarCarrosselIndividual('carrosselHobby',            '12', 4,  'hobby', estabelecimento.hobby);
+    const carrosselCartao           = criarCarrosselIndividual('carroselCartao',            '10', 6,  'cartao', estabelecimento.cartao);
+    const carrosselDias             = criarCarrosselIndividual('carrosselDias',             '9', 8,  'dias', estabelecimento.dias);
+    const carrosselHora             = criarCarrosselIndividual('carrosselHora',             '6', 8,  'hora', estabelecimento.hora);
+    const carrosselEstiloServico    = criarCarrosselIndividual('carrosselEstiloServico',    '12', 4,  'estilo serviço', estabelecimento.estilo_servico);
+
     return `
     <div class="container">
         <h1>${estabelecimento.nome}</h1>
@@ -120,7 +243,33 @@ function criarCardDetalhe(estabelecimento) {
                 <p>${estabelecimento.lactosefree === 'Sim' ? '<i class="fas fa-check"></i> Sim' : '<i class="fas fa-times"></i> Não'}</p>
             </div>
         </div>
-        
+
+        <br><h2>funcionamento</h2>
+        <h5>dias</h5>
+        <p>${carrosselDias} </p>
+        <h5>horário</h5>
+        <p>${carrosselHora} </p>
+
+        <br><h2>detalhes gerais</h2>
+        <h5>culinária</h5>
+        <p>${carrosselCozinha} </p>
+        <h5>estilo serviço</h5>
+        <p>${carrosselEstiloServico} </p>
+        <h5>tipo de evento</h5>
+        <p>${carrosselTipoEvento} </p>
+        <h5>estilo musical</h5>
+        <p>${carrosselEstiloMusical} </p>
+        <h5>caracteristicas locais</h5>
+        <p>${carrosselLocal} </p>
+        <h5>ambiente</h5>
+        <p>${carrosselAmbiente} </p>
+        <h5>hobby</h5>
+        <p>${carrosselHobby} </p>
+
+        <br><h2>formas de pagamento</h2>
+        <h5>cartões aceitos</h5>
+        <p>${carrosselCartao} </p>
+
         <br><h2>localização</h2>
         <p><strong>endereço:</strong> ${estabelecimento.rua}, ${estabelecimento.bairro}, ${estabelecimento.cidade}, ${estabelecimento.cep} </p>
         <p><strong>bairro:</strong> ${estabelecimento.bairro} </p>
@@ -129,7 +278,6 @@ function criarCardDetalhe(estabelecimento) {
         <br><h2>contato</h2>
 
         <br><h2>fotos</h2>
-        <br>
     </div>
     `;
 }
--- public/script/fotos.js
+++ public/script/fotos.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     loadNavbar();
     loadFooter();
+    fetchLugares();
 });
 
 function loadNavbar() {
@@ -23,10 +24,6 @@ function loadFooter() {
             console.error('Falha ao carregar o footer:', error);
         });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    fetchLugares();
-});
 
 function fetchLugares() {
     fetch('http://localhost:3000/api/lugares')
@@ -55,27 +52,58 @@ function bufferToBase64(buf) {
 
 function carregarFotos() {
     const lugarId = document.getElementById('lugarSelect').value;
-    const fotosContainer = document.getElementById('fotosContainer');
-    fotosContainer.innerHTML = '';
+    const carouselContainer = document.getElementById('carouselContainer');
+    carouselContainer.innerHTML = '';
 
     fetch(`http://localhost:3000/api/fotos-lugares/${lugarId}`)
         .then(response => response.json())
         .then(data => {
             if (data && data.length > 0) {
+                let carouselIndicators = '';
+                let carouselInner = '';
+                let fotoIndex = 0; // Índice para cada foto
+
                 data.forEach(item => {
                     item.fotos.forEach(foto => {
-                        const img = document.createElement('img');
-                        img.src = `data:${foto.contentType};base64,${bufferToBase64(foto.data.data)}`;
-                        img.classList.add('foto-img');
-                        fotosContainer.appendChild(img);
+                        const base64String = bufferToBase64(foto.data.data);
+                        const isActive = fotoIndex === 0 ? 'active' : ''; // Apenas a primeira foto é 'active'
+                        
+                        carouselIndicators += `<li data-target="#fotosCarousel" data-slide-to="${fotoIndex}" class="${isActive}"></li>`;
+                        carouselInner += `
+                            <div class="carousel-item ${isActive}">
+                                <img class="d-block w-100" src="data:${foto.contentType};base64,${base64String}">
+                            </div>`;
+                        
+                        fotoIndex++;
                     });
                 });
+
+                carouselContainer.innerHTML = `
+                    <div id="fotosCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            ${carouselIndicators}
+                        </ol>
+                        <div class="carousel-inner">
+                            ${carouselInner}
+                        </div>
+                        <a class="carousel-control-prev" href="#fotosCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#fotosCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>`;
+
+                // Inicializar o carrossel
+                $('#fotosCarousel').carousel();
             } else {
-                fotosContainer.innerHTML = '<p>Nenhuma foto encontrada para este lugar.</p>';
+                carouselContainer.innerHTML = '<p>Nenhuma foto encontrada para este lugar.</p>';
             }
         })
         .catch(error => {
             console.error('Erro ao carregar fotos:', error);
-            fotosContainer.innerHTML = '<p>Erro ao carregar fotos.</p>';
+            carouselContainer.innerHTML = '<p>Erro ao carregar fotos.</p>';
         });
 }
