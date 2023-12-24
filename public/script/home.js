d2ff69550a19759281ff7d57f958a9c1f261b0bd
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-12-24 11:02:53 -0300

    Atualização

--- public/script/home.js
+++ public/script/home.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     loadNavbar();
     loadFooter();
     initializeFormOptions();
+    carregarFotos()
 });
 
 function loadNavbar() {
@@ -121,3 +122,70 @@ document.getElementById('search-form').addEventListener('submit', function(event
     // Redirecionar para a página de estabelecimentos com os parâmetros da query string
     window.location.href = `estabelecimentos.html?${queryParams.toString()}`;
 });
+
+function carregarFotos() {
+    const carouselContainer = document.getElementById('carouselNovidades');
+    carouselContainer.innerHTML = '';
+
+    fetch(`/api/fotos-lugares/658799aeb6c19129dbbc5d1d`)
+        .then(response => response.json())
+        .then(data => {
+            if (data && data.length > 0) {
+                let carouselIndicators = '';
+                let carouselInner = '';
+                let fotoIndex = 0; // Índice para cada foto
+
+                data.forEach(item => {
+                    item.fotos.forEach(foto => {
+                        const base64String = bufferToBase64(foto.data.data);
+                        const isActive = fotoIndex === 0 ? 'active' : ''; // Apenas a primeira foto é 'active'
+                        
+                        carouselIndicators += `<li data-target="#fotosCarousel" data-slide-to="${fotoIndex}" class="${isActive}"></li>`;
+                        carouselInner += `
+                            <div class="carousel-item ${isActive}">
+                                <img class="d-block w-100" src="data:${foto.contentType};base64,${base64String}">
+                            </div>`;
+                        
+                        fotoIndex++;
+                    });
+                });
+
+                carouselContainer.innerHTML = `
+                    <div id="fotosCarousel" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            ${carouselIndicators}
+                        </ol>
+                        <div class="carousel-inner">
+                            ${carouselInner}
+                        </div>
+                        <a class="carousel-control-prev" href="#fotosCarousel" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#fotosCarousel" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>`;
+
+                // Inicializar o carrossel
+                $('#fotosCarousel').carousel();
+            } else {
+                carouselContainer.innerHTML = '<p>Nenhuma foto encontrada para este lugar.</p>';
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao carregar fotos:', error);
+            carouselContainer.innerHTML = '<p>Erro ao carregar fotos.</p>';
+        });
+}
+
+function bufferToBase64(buf) {
+    let binary = '';
+    const bytes = new Uint8Array(buf);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+        binary += String.fromCharCode(bytes[i]);
+    }
+    return window.btoa(binary);
+}
