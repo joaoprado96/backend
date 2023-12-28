bf2fc2302d6c8516214cfb0294ffec0754e1b6a3
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-12-28 14:40:45 -0300

    Atualização

--- public/script/login.js
+++ public/script/login.js
@@ -1,29 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    loadNavbar();
-    loadFooter();
-});
-
-function loadNavbar() {
-    const navbarPlaceholder = document.getElementById('navbar-placeholder');
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(html => {
-            navbarPlaceholder.innerHTML = html;
-        }).catch(error => {
-            console.error('Falha ao carregar o navbar:', error);
-        });
-}
-function loadFooter() {
-    const footerPlaceholder = document.getElementById('footer-placeholder');
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(html => {
-            footerPlaceholder.innerHTML = html;
-        }).catch(error => {
-            console.error('Falha ao carregar o footer:', error);
-        });
-}
-
 document.getElementById('registerForm').addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -46,12 +20,23 @@ document.getElementById('registerForm').addEventListener('submit', function(e) {
       return response.json().then(text => { throw new Error(text.message || 'Falha no Login') });
   })
   .then(data => {
-      alert(data.message); // Mostra uma mensagem de alerta com a mensagem do servidor
-      localStorage.setItem('token', data.token); // Salva o token no localStorage
-      window.location.href = '/home.html'; // Redireciona para a página desejada
-  })
+        var popup = document.getElementById('notificationPopupSucesso');
+        popup.style.display = 'block';
+
+        // Espera 3 segundos antes de redirecionar
+        setTimeout(function() {
+            popup.style.display = 'none';
+            localStorage.setItem('token', data.token); // Salva o token no localStorage
+            window.location.href = '/home.html'; // Redireciona para a página desejada    
+        }, 4500);
+})
   .catch(error => {
-      console.error('Erro no login:', error);
-      alert('Erro no login: ' + error.message);
+    var popup = document.getElementById('notificationPopupFalha');
+    popup.style.display = 'block';
+
+    // Espera 3 segundos antes de redirecionar
+    setTimeout(function() {
+        popup.style.display = 'none';
+    }, 4500);
   });
 });
--- public/script/usuario.js
+++ public/script/usuario.js
@@ -1,29 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    loadNavbar();
-    loadFooter();
-});
-
-function loadNavbar() {
-    const navbarPlaceholder = document.getElementById('navbar-placeholder');
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(html => {
-            navbarPlaceholder.innerHTML = html;
-        }).catch(error => {
-            console.error('Falha ao carregar o navbar:', error);
-        });
-}
-function loadFooter() {
-    const footerPlaceholder = document.getElementById('footer-placeholder');
-    fetch('footer.html')
-        .then(response => response.text())
-        .then(html => {
-            footerPlaceholder.innerHTML = html;
-        }).catch(error => {
-            console.error('Falha ao carregar o footer:', error);
-        });
-}
-
 document.getElementById('registerForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
