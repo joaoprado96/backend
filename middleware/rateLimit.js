304b11a57dcafe8c09edbaa0d8ea71aa312b5e8b
João Prado <58610872+joaoprado96@users.noreply.github.com>
2023-12-29 09:13:20 -0300

    Ajuste do RateLimit

--- middleware/rateLimit.js
+++ middleware/rateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Crie uma instância do middleware de rate limiting
 const limiter = rateLimit({
     windowMs: 1000, // 1 segundo
-    max: 3, // limite cada IP a 10 requisições por janela de 'windowMs'
+    max: 10, // limite cada IP a 10 requisições por janela de 'windowMs'
     keyGenerator: (req /*, res*/) => {
         // Aqui, você pode extrair o token JWT do cabeçalho de autorização e usá-lo como chave
         return req.headers.authorization; // Assumindo que o token esteja no cabeçalho Authorization
