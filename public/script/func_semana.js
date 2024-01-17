55dcae5e33c649a32fa5b3efcbf42bdd821f041f
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-01-17 14:18:27 -0300

    Atualizar cadastro

--- public/script/func_semana.js
+++ public/script/func_semana.js
@@ -1,5 +1,5 @@
 function ObterDiaSemana() {
     let hoje = new Date();
-    let diasDaSemana = ["domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sabado"];
+    let diasDaSemana = ["domingo", "segunda-feira", "terca-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sabado"];
     diaDaSemanaGlobal = diasDaSemana[hoje.getDay()];
 }
