5075b67b3dbf8d1e121fea2a99482eb7fc17e977
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-01-27 18:01:54 -0300

    Ajuste

--- public/script/navbar.js
+++ public/script/navbar.js
@@ -43,14 +43,14 @@ function criarNavbar() {
         navOpenBtn = document.querySelector(".navOpenBtn"),
         navCloseBtn = document.querySelector(".navCloseBtn");
 
-    searchIcon.addEventListener("click", () => {
-        nav.classList.toggle("openSearch");
-        nav.classList.remove("openNav");
-        if (nav.classList.contains("openSearch")) {
-            return searchIcon.classList.replace("uil-search", "uil-times");
-        }
-        searchIcon.classList.replace("uil-times", "uil-search");
-    });
+    // searchIcon.addEventListener("click", () => {
+    //     nav.classList.toggle("openSearch");
+    //     nav.classList.remove("openNav");
+    //     if (nav.classList.contains("openSearch")) {
+    //         return searchIcon.classList.replace("uil-search", "uil-times");
+    //     }
+    //     searchIcon.classList.replace("uil-times", "uil-search");
+    // });
 
     navOpenBtn.addEventListener("click", () => {
         nav.classList.add("openNav");
