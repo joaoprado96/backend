8eeff2da873aee172b5dbfc41edd3e57d702cab0
João Prado <58610872+joaoprado96@users.noreply.github.com>
2024-01-28 12:21:50 -0300

    Atualização

--- routes/fotosLugaresRoutes.js
+++ routes/fotosLugaresRoutes.js
@@ -19,7 +19,6 @@ router.get('/fotos-lugares/:lugarId', fotosLugaresController.obterFotosPorLugar)
 // Rota para obter a primeira foto de um determinado lugar
 router.get('/primeira-foto-lugar/:lugarId', fotosLugaresController.obterPrimeiraFotoPorLugar);
 
-
 // Rota para deletar um LugarId e suas fotos
 router.delete('/fotos-lugares/:lugarId',rateLimit, verificarToken.verificarToken, fotosLugaresController.deletarLugarEFotos);
 
